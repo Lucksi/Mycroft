1796642a52c2e4b73e1596e3177fd465b60c2e98
Luca Garofalo <79308097+Lucksi@users.noreply.github.com>
2025-06-18 04:19:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # ORIGINAL CREATOR: Luca Garofalo (Lucksi)
 # AUTHOR: Luca Garofalo (Lucksi)
-# Copyright (C) 2024 Lucksi <lukege287@gmail.com>
+# Copyright (C) 2024-2025 Lucksi <lukege287@gmail.com>
 # License: GNU General Public License v3.0
 
 from Core import Menu
+from Core.Utils import Database
 
 if __name__ == "__main__":
     try:
+        Database.DATABASE.CREATE()
         Menu.MAIN.Static()
     except KeyboardInterrupt:
         print("\n\nCtrl c detected Quitting the Program\n")
